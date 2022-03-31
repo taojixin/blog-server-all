539227f13be84664591e0755f6935b72ca7cd5bf
Tao <491675919@qq.com>
2022-03-31 21:10:30 +0800

    2022.3.31

--- blog-native/src/controller/blog.js
+++ blog-native/src/controller/blog.js
@@ -1,5 +1,6 @@
 const { exec } = require('../db/mysql')
 
+// 1.获取博客列表
 const getList = (author, keyword) => {
   let sql = `select * from blogs where 1=1 `
   if (author) {
@@ -14,35 +15,80 @@ const getList = (author, keyword) => {
   return exec(sql)
 }
 
+// 2.获取博客详情
 const getDetail = (id) => {
-  return [
-    {
-      id: 1,
-      title: '标题a',
-      content: '内容1',
-      createTime: 1546610491112,
-      author: 'zhangsan'
-    },
-  ]
+  const sql = `select * from blogs where id='${id}'`
+  return exec(sql).then(rows => {
+    return rows[0]  // 这里这样写的原因：因为查询返回的是数组，这里只查询一个，数组中只有一个内容，所以提取出来这里返回对象
+  })
 }
 
+
+// 3.新建一篇博客
 const newBlog = (blogData = {}) => {
   // blogData 是一个博客对象，包括title content等
-  return {
-    id: 3 // 表示新建成功，返回保存的blog在数据库中的id
-  }
+  const title = blogData.title
+  const content = blogData.content
+  const author = blogData.author
+  const createTime = Date.now()
+
+  const sql = `INSERT INTO blogs (title,content,createtime,author) VALUE ('${title}', '${content}', ${createTime}, '${author}')`
+  return exec(sql).then(insertData => {
+    // console.log(insertData); // 输出为：
+    // OkPacket {
+    //   fieldCount: 0,
+    //   affectedRows: 1,
+    //   insertId: 3,
+    //   serverStatus: 2,
+    //   warningCount: 0,
+    //   message: '',
+    //   protocol41: true,
+    //   changedRows: 0
+    // }
+    return {
+      id: insertData.insertId
+    }
+  })
 }
 
+// 4.更新一篇博客
 const updateBlog = (id, blogData = {}) => {
   // id 就是更新博客的id
   // blogData是一个博客对象， 包括title content等
-  console.log(id, blogData);
-  return true
+  const title = blogData.title
+  const content = blogData.content
+  const sql = `update blogs set title='${title}', content='${content}' where id=${id}`
+
+  return exec(sql).then(updateData => {
+    // console.log(updateData); // 返回：
+    // OkPacket {
+    //   fieldCount: 0,
+    //   affectedRows: 1,
+    //   insertId: 0,
+    //   serverStatus: 2,
+    //   warningCount: 0,
+    //   message: '(Rows matched: 1  Changed: 1  Warnings: 0',
+    //   protocol41: true,
+    //   changedRows: 1
+    // }
+    if (updateData.affectedRows > 0) {
+      return true
+    }
+    return false
+  })
 }
 
-const delBlog = (id) => {
+// 5.删除一篇博客
+const delBlog = (id, author) => {
   // id 为要删除的id
-  return true
+  const sql = `delete from blogs where id='${id}' and author='${author}'`
+  return exec(sql).then(delData => {
+    console.log(delData);
+    if (delData.affectedRows > 0) {
+      return true
+    }
+    return false
+  })
 
 }
 
--- blog-native/src/router/blog.js
+++ blog-native/src/router/blog.js
@@ -19,39 +19,49 @@ const handleBlogRouter = (req, res) => {
     
   }
 
-  // // 2.获取博客详情
-  // if (method === 'GET' && req.path === '/api/blog/detail') {
-  //   const id = req.query.id
-  //   const data = getDetail(id)
-  //   return new SuccessModel(data)
-  // }
+  // 2.获取博客详情
+  if (method === 'GET' && req.path === '/api/blog/detail') {
+    const result = getDetail(id)
+    return result.then(data => {
+      return new SuccessModel(data)
+    })
+  }
 
-  // // 3.新建一篇博客
-  // if (method === 'POST' && req.path === '/api/blog/new') {
-  //   // req.body 为post请求携带的数据
-  //   const data = newBlog(req.body)
-  //   return new SuccessModel(data)
-  // }
+  // 3.新建一篇博客
+  if (method === 'POST' && req.path === '/api/blog/new') {
+    // req.body 为post请求携带的数据
+    req.body.author = 'tjx' // 假数据，
+    const result = newBlog(req.body)
+    return result.then(data => {
+      return new SuccessModel(data)
+    })
+  }
 
-  // // 4.更新一篇博客
-  // if (method === 'POST' && req.path === '/api/blog/update') {
-  //   const result = updateBlog(id, req.body)
-  //   if (result) {
-  //     return new SuccessModel()
-  //   } else {
-  //     return new ErrorModel("更新博客失败")
-  //   }
-  // }
+  // 4.更新一篇博客
+  if (method === 'POST' && req.path === '/api/blog/update') {
+    const result = updateBlog(id, req.body)
+    return result.then(val => {
+      if (val) {
+        return new SuccessModel()
+      } else {
+        return new ErrorModel('更新博客失败')
+      }
+    })
+  }
 
-  // // 5.删除一篇博客
-  // if (method === 'POST' && req.path === 'api/blog/del') {
-  //   const result = delBlog(id)
-  //   if (result) {
-  //     return new SuccessModel()
-  //   } else {
-  //     return new ErrorModel("更新博客失败")
-  //   }
-  // }
+  // 5.删除一篇博客
+  if (method === 'POST' && req.path === '/api/blog/del') {
+    const author = 'zhangsan'
+    const result = delBlog(id, author)
+    console.log(id);
+    return result.then(val => {
+      if (val) {
+        return new SuccessModel()
+      } else {
+        return new ErrorModel("删除博客失败")
+      }
+    })
+  }
 }
 
 module.exports = handleBlogRouter
